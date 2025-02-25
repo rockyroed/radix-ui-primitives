5d06e808087c96ac2a77b1f21f1ba9e985739a0a
roed <108738926+rockyroed@users.noreply.github.com>
2025-02-25 10:31:41 +0800

    Add TODO

--- packages/react/select/src/select.tsx
+++ packages/react/select/src/select.tsx
@@ -128,6 +128,7 @@ const Select: React.FC<SelectProps> = (props: ScopedProps<SelectProps>) => {
   });
   const triggerPointerDownPosRef = React.useRef<{ x: number; y: number } | null>(null);
 
+  // TODO: Fix unwanted form control
   // We set this to true by default so that events bubble to forms without JS (SSR)
   const isFormControl = trigger ? form || !!trigger.closest('form') : true;
   const [nativeOptionsSet, setNativeOptionsSet] = React.useState(new Set<NativeOption>());
